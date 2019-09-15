28afd0589ab9c8b830070e5b1a5f7c1132011a20
Bio Lab Analytics <34093743+Cyberskout99@users.noreply.github.com>
2019-09-15 16:30:11 -0700

    Cyberskout99 patch 2 1 (#21)
    * Updated Dummy Data for Bio_tbl
    
    Added StaffID_fk entry to CSV for biometrics data table
    
    * Update pandapp.py, create models.py (#19)
    
    "Models" from the original pandapp.py have been abstracted to a new file (models.py), per Eugene's recommendation on Slack channel
    
    pandapp.py has been updated with other fixes to allow for data entry
    
    Next step is updating pandapp.py to call in various classes for data entry from the 'models.py' file.

--- pandapp.py
+++ pandapp.py
@@ -1,96 +1,38 @@
-import psycopg2
+# import psycopg2
+from flask import request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from models import Bio
+from config import Config
 
-pandapp = Flask(__name__)
+app = Flask(__name__)  # by default, takes name of root directory
+app.config.from_object(Config)
 
-pandapp.config['DEBUG']= True
+from app import routes
 
-pandapp.config.update(               ## dictionary of arguments to pass through for config settings
-    SECRET_KEY='Battlestar*84',
-    SQLALCHEMY_DATABASE_URI='postgresql://postgres:Battlestar*84@localhost/PandaApp_Db',
-    SQLALCHEMY_TRACK_MODIFICATIONS=False)
+dummyStaff = {'StaffID': 1, 'StaffFirst': 'Jim', 'StaffLast': 'Jones', 'Username': 'UserID123', 'Password': 'TopSecret'}
 
-db = SQLAlchemy(pandapp) ## create instance of SQLAlchemy class
-
-##  Basic Panda Info (PandaID is a dependency for biometrics entry) ##
-class Panda(db.Model):
-    __tablename__ = 'Panda_tbl'
-    PandaID = db.Column('PandaID', db.Integer, primary_key=True)
-    PandaName = db.Column('Name', db.Unicode) ## not null
-    PandaSex = db.Column('Sex', db.Unicode) ## not null
-    DOB = db.Column('DOB', db.DateTime) ## not null
-    DODeath = db.Column('DODeath', db.DateTime)
-    Provenance = db.Column('Provenance', db.Unicode)
-    CaptureDate = db.Column('Capture_Date', db.Date)
-    BirthLoc = db.Column('Birth_Location', db.Unicode)
-    biometrics = db.relationship('Bio', backref='panda', lazy=True)
-
-##  Data Collection Staff Info (StaffID is a dependency for biometrics entry) ##
-class Staff(db.Model):
-    __tablename__ = 'Staff_tbl'
-    StaffID = db.Column('StaffID', db.Integer, primary_key=True)
-    StaffFirst = db.Column('StaffFirst', db.Unicode)
-    StaffLast = db.Column('StaffLast', db.Unicode)
-    Username = db.Column('Username', db.Unicode)
-    Password = db.Column('Password', db.Unicode)
-    biometrics = db.relationship('Bio', backref='staff', lazy=True)
+#  Apply decorator to 'bio_entry()' function #
 
 
-##  Define a "bio_entry" API (point) for the app ##
-@pandapp.route('/bio_entry', methods=('GET', 'POST'))
-
-##  Define data for Flask/SQLAlchemy 'Bio' data object (class) ##
-class Bio(db.Model):
-    __tablename__ = 'Bio_tbl'
-    BioID = db.Column('BioID', db.Integer, primary_key=True)
-    PandaID_fk = db.Column('PandaID_fk', db.Integer) ## FK relationship
-    BodyLength = db.Column('BodyLength', db.Integer)
-    BodyHeight = db.Column('BodyHeight', db.Integer)
-    BodyWeight = db.Column('BodyWeight', db.Integer)
-    BioDate = db.Column('BioDate', db.DateTime) ## MM/DD/YYYY
-    BioTime = db.Column('BioTime', db.DateTime) ## HH:MM - 24 hour ideally
-    StaffID_fk = db.Column('StaffID_fk', db.Integer)
-
-    def __init__(self, BioID, pandaid, length, height, weight, biodate, biotime, staffid): ## Is this correct?
-        self.BioID = BioID
-        self.PandaID_fk = pandaid
-        self.BodyLength = length
-        self.BodyHeight = height
-        self.BodyWeight = weight
-        self.BioDate = biodate
-        self.BioTime = biotime
-        self.StaffID_fk = staffid
-
-##  Define request of form data for 'bio_entry()' function ##
+@app.route('/bio_entry', methods=('GET', 'POST'))
 def bio_entry():
     if request.method == 'POST':
-        pandaid = request.form['PandaID']
-        weight = request.form['Weight']
-        height = request.form['Height']
-        length = request.form['Length']
-        biodate = request.form['BioDate']
-        biotime = request.form['BioTime']
-        staffid = request.form['StaffID']
-        error = None
+        db.PandaID = request.form['PandaID']
+        db.Weight = request.form['Weight']
+        db.Height = request.form['Height']
+        db.Length = request.form['Length']
+        db.BioDate = request.form['BioDate']
+        db.BioTime = request.form['BioTime']
+        db.StaffID = request.form['StaffID']
+        db.BioID = request.form['BioID']
 
-        if not title:
-            error = 'PandaID is required.'
+    return render_template('Bio_Entry.html') ## add validation for successful form submission prior to render template
 
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'INSERT INTO bio_tbl (PandaID_fk, BodyWeight, BodyHeight, BodyLength, BioDate, BioTime, StaffID_fk)'
-                'VALUES (?, ?, ?, ?, ?, ?, ?)',
-                (pandaid, weight, height, length,biodate, biotime, staffid)
-            )
-            db.commit()
-            return redirect(url_for('pandapp.index'))
-
-    return render_template('pandapp/Template/Bio_Entry.html')
+# from pandapp import routes # doesn't seem to be working ? #
+# Where does the "Class" come into play here?   Re-read tutorial.  Study (1) Classes, (2) Decorators
+Bio.BioID = 10
 
 if __name__ == '__main__':
     db.create_all()
-    pandapp.run(debug=True)
+    app.run(debug=True)
